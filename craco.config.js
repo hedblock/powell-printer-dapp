757b3aded24cf3e4adf136bbe44aeea2427bd247
jason <jason@hedmans.org>
2022-01-09 14:40:15 -0500

    changed primary blue

--- craco.config.js
+++ craco.config.js
@@ -7,7 +7,7 @@ module.exports = {
             options: {
                 lessLoaderOptions: {
                     lessOptions: {
-                        modifyVars: { '@primary-color': '#4367AA' },
+                        modifyVars: { '@primary-color': '#2775CA' },
                         javascriptEnabled: true,
                     },
                 },
